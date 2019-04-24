139a2d78f3f596ed3ea395e159ab747b5e64909b
Nataliya_Shchukina <Nataliya_Shchukina@epam.com>
2019-04-24 21:38:24 +0300

    new changes 24.04.2019

--- src/com/epam/nataliya_shchukina/lesson1/task1/Application.java
+++ src/com/epam/nataliya_shchukina/lesson1/task1/Application.java
@@ -2,7 +2,6 @@ package com.epam.nataliya_shchukina.lesson1.task1;
 public class Application {
 
     public int [] arrayInitionalization( int size, int leftBorder, int rightBorder){
-        //int size =20;
         int[] array = new int[size];
         for (int i = 0; i < size; i++) {
             array[i] = (int) (Math.random() * (rightBorder-leftBorder + 1))+leftBorder;
@@ -17,11 +16,14 @@ public class Application {
         System.out.println("Original array:");
         arrayProcessing.arrayOutput(array);
         System.out.println();
+
         var negativeIndex = arrayProcessing.getMaxNegativeIndex(array);
         System.out.println("Maxotr: " + array[negativeIndex] + " [" + negativeIndex + "]");
-        var pozitiveIndex = arrayProcessing.getMinPozitiveIndex(array);
-        System.out.println("Minpol: " + array[pozitiveIndex] + " [" + pozitiveIndex + "]");
-        var newarray = arrayProcessing.changeElementInArray(array,negativeIndex,pozitiveIndex);
+
+        var positiveIndex = arrayProcessing.getMinPositiveIndex(array);
+        System.out.println("Minpol: " + array[positiveIndex] + " [" + positiveIndex + "]");
+
+        var newarray = arrayProcessing.changeElementInArray(array,negativeIndex,positiveIndex);
         System.out.println("Modified array:");
         arrayProcessing.arrayOutput(newarray);
     }
--- src/com/epam/nataliya_shchukina/lesson1/task1/ArrayProcessing.java
+++ src/com/epam/nataliya_shchukina/lesson1/task1/ArrayProcessing.java
@@ -1,46 +1,42 @@
 package com.epam.nataliya_shchukina.lesson1.task1;
 
 public class ArrayProcessing {
-
+    //вывод массива на экран
     public void arrayOutput(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "[" + i + "]" + " ");
         }
     }
-
+     //получаем индекс максимального отрацательного цисла массива
     public int getMaxNegativeIndex(int[] array) {
-        boolean isFound = false;
-        int maxotr = array[0];
+        int maxotr = Integer.MIN_VALUE + 1;
         int kmax = 0;
         for (int i = 0; i < array.length; i++) {
-            if ((Math.abs(array[i])<= Math.abs(maxotr) ) & (array[i] < 0)){
+            if ((Math.abs(array[i])< Math.abs(maxotr)) & (array[i] < 0)){
                 maxotr = array[i];
                 kmax = i;
-                isFound = true;
             }
         }
         return kmax;
     }
-
-    public int getMinPozitiveIndex(int[] array) {
-        boolean isFound = false;
-        int minpol = array[0];
-        int lmax = 0;
+    //получаем индекс минимального положительного цисла массива
+    public int getMinPositiveIndex(int[] array) {
+        int minpol = Integer.MAX_VALUE;
+        int lmin = 0;
 
         for (int i = 0; i < array.length; i++) {
-          if ((Math.abs(array[i]) <= Math.abs(minpol)) & (array[i] > 0)) {
+          if((array[i] < minpol) & (array[i] > 0)) {
                 minpol = array[i];
-                lmax = i;
-                isFound = true;
+                lmin = i;
             }
         }
-        return lmax;
+        return lmin;
     }
-
+     //меняем элементы массива
     public int[] changeElementInArray(int[] array, int k, int l) {
-        int temparray1 = array[k];
+        int temparray = array[k];
         array[k] = array[l];
-        array[l] = temparray1;
+        array[l] = temparray;
         return array;
     }
     }
